0023f15fcb9a7ec34fdace97256749565ac8100e
syaidinaarafhan <qwudtile@gmail.com>
2026-01-26 21:41:42 +0700

    fixxing baseUrl to backend

--- src/lib/axios/axios.js
+++ src/lib/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:3005",
+    baseURL: import.meta.env.VITE_API_URL || "http://localhost:3005",
     withCredentials: true,
     timeout: 10000,
     headers: {
